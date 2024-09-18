63d98d71cb7a8ee7186747401cb851aa3ed9c669
SubaniMalla <subanimalla1@gmail.com>
2024-09-18 17:18:27 +0545

    Add rotate file

---
+++ rotate.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import random
+import imageio.v2 as imageio
+
+
+image_path = r'C:\Users\subani\OneDrive\Desktop\codewave_cookie\coin_og.jpg'
+image = imageio.imread(image_path)
+
+def rotate_image(image, angle):
+    rows, cols, _ = image.shape
+    M = cv2.getRotationMatrix2D((cols // 2, rows // 2), angle, 1)
+    
+  
+    return cv2.warpAffine(image, M, (cols, rows), borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
+
+
+synthetic_images = []
+for _ in range(10):  
+   
+    angle = random.uniform(-45, 45)
+    synthetic_img = rotate_image(image, angle)
+    
+    synthetic_images.append(synthetic_img)
+
+
+for i, img in enumerate(synthetic_images):
+    imageio.imwrite(f'C:/Users/subani/OneDrive/Desktop/codewave_cookie/rotate/rotate_synthetic_{i}.jpg', img)
+
+
